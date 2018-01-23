2fefb1ecdd743ddb824dbc3609446e55836aa981
Beyhan Esen <beyhan@botanalytics.co>
2018-01-23 13:49:21 +0300

    Botkit watson, facebook.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const MicrosoftBotFramework = require('./platforms/microsoft');
 const FacebookMessenger = require('./platforms/facebook');
 const GoogleAssistant = require('./platforms/google');
-const AmazonAlexa = require('./platforms/amazon');
+const AmazonAlexa = require('./platforms/amazon.alexa');
+const AmazonLex = require('./platforms/amazon.lex');
+const BotkitMiddlewares = require('./platforms/botkit');
 const Viber = require('./platforms/viber');
 const Kik = require('./platforms/kik');
 
@@ -9,5 +11,7 @@ module.exports.MicrosoftBotFramework = MicrosoftBotFramework;
 module.exports.FacebookMessenger = FacebookMessenger;
 module.exports.GoogleAssistant = GoogleAssistant;
 module.exports.AmazonAlexa = AmazonAlexa;
+module.exports.AmazonAlexa = AmazonLex;
+module.exports.BotkitMiddlewares = BotkitMiddlewares;
 module.exports.Viber = Viber;
 module.exports.Kik = Kik;
