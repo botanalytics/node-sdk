d4abd6babb1b4afc70c105dd9883454ba99a6acd
Beyhan Esen <beyhan@botanalytics.co>
2018-01-30 17:00:52 +0300

    Slack event api updates.

--- platforms/slack.event.api.js
+++ platforms/slack.event.api.js
@@ -1,77 +1,125 @@
 'use strict';
 const util = require('util');
-module.exports = function (token, userConfig) {
+const req = require('request');
+
+module.exports = function (token, slackBotToken, userConfig) {
     // Check token
-    if (!token) {
-
+    if(!token || token.constructor !== String)
         throw new Error('You must provide a Botanalytics token!');
-    }
-
-    // Create default config
-    const config = {
-        baseUrl: 'https://api.botanalytics.co/v1/',
-        debug: false
-    };
+    this.token = token;
+    //check slack token
+    if(!slackBotToken || slackBotToken.constructor !== String)
+        throw new Error('You must provide a Slack bot token!');
+    this.slackBotToken = slackBotToken;
     // Merge user configuration into the default config
-    Object.assign(config, userConfig);
+    this._config = Object.assign({debug: false}, userConfig);
+    //state definers
+    this._isInited = false;
+    this._isFetched = false;
+    //logger
+    this.logger = new require('../../util').Logger(this._config);
+    this.logger.debug('Logging enabled.');
+    this.logger.debug('Configuration: ' + util.inspect(this._config));
 
-    const log = new require('../../util').Logger(config);
-    log.debug('Logging enabled.');
-    log.debug('Configuration: ' + util.inspect(config));
+    this._init = function (data) {
 
-    // Configure request defaults
-    const request = require('request').defaults({
-        baseUrl: config.baseUrl,
-        headers: {
-            'Authorization': 'Token ' + encodeURIComponent(token),
-            'Content-Type': 'application/json'
-        }
-    });
+        if(!this._isInited){
+            //context referencing
+            const self = this;
+            //send
+            req({
 
-    return {
-
-        log: function (message) {
-
-            if (!message || message.constructor !== Object) {
-
-                log.error('Message object is not provided!');
-                return;
-            }
-
-            if(message.type === "url_verification" || message.type !== "event_callback"){
-                log.debug(`Ignoring ${message.type} message.`);
-                return;
-            }
-
-            //log incoming message
-            log.debug('Logging incoming message...\n' + util.inspect(message));
-            // create copy of the message
-            const payload = Object.assign({}, message);
-            request({
-
-                url: '/messages/slack/eventapi',
+                url: 'https://api.botanalytics.co/v1/bots/slack/initialize/',
                 method: 'POST',
                 json: true,
+                headers: {
+                    'Authorization': 'Token '+encodeURIComponent(self.token),
+                    'Content-Type': 'application/json'
+                },
                 body: {
-                    message: payload
+                    message: JSON.parse(data)
                 }
 
             }, (err, resp, payload) => {
 
-                if (err) {
+                if(err) {
+                    self.logger.error('Failed to log init message.', err);
+                    self.logger.debug('Retrying...');
+                    setTimeout(self.init.bind(self, data), 10000);
+                }
+                else{
+                  self._isInited = true;
+                }
 
-                    log.error('Failed to log incoming message.', err);
+            });
+        }
+    };
+    this._fetch = function () {
+        if(!this._isFetched){
+            const self = this;
+            // check rtm start
+            req.post({url : "https://slack.com/api/rtm.start", form : {token : self.slackBotToken}}, function (err, resp, body) {
 
-                } else {
-
-                    err = log.checkResponse(resp, 'Successfully logged incoming message.', 'Failed to log incoming message.');
-
-                    if (err) {
-
-                        log.error('Failed to log incoming message.', err);
-                    }
+                if(JSON.parse(body).ok === false)
+                    setTimeout(self._fetch.bind(self), 10000);
+                else {
+                    self._init(body);
+                    self._isFetched = true;
                 }
             });
         }
-    }
+    };
+    //init info message
+    this._fetch();
+
+    this.log = function (message) {
+
+        if (!message || message.constructor !== Object) {
+
+            this.logger.error('Message object is not provided!');
+            return;
+        }
+
+        if(message.type === "url_verification" || message.type !== "event_callback"){
+            this.logger.debug(`Ignoring ${message.type} message.`);
+            return;
+        }
+        const self = this;
+        //log incoming message
+        self.logger.debug('Logging incoming message...\n' + util.inspect(message));
+        // create copy of the message
+        const payload = Object.assign({}, message);
+        //send
+        req({
+
+            url: 'https://api.botanalytics.co/v1/messages/slack/eventapi',
+            method: 'POST',
+            json: true,
+            headers: {
+                'Authorization': 'Token ' + encodeURIComponent(self.token),
+                'Content-Type': 'application/json'
+            },
+            body: {
+                message: payload
+            }
+
+        }, (err, resp, payload) => {
+
+            if (err) {
+
+                self.logger.error('Failed to log incoming message.', err);
+
+            } else {
+
+                err = self.logger.checkResponse(resp, 'Successfully logged incoming message.', 'Failed to log incoming message.');
+
+                if (err) {
+
+                    self.logger.error('Failed to log incoming message.', err);
+                }
+            }
+        });
+    };
+    
+    return this;
 };
