92a4f234c07ccc949626d3c8f306b081fdd31b03
Beyhan Esen <beyhan@botanalytics.co>
2018-01-31 14:09:39 +0300

    Config.

--- platforms/slack.event.api.js
+++ platforms/slack.event.api.js
@@ -12,14 +12,17 @@ module.exports = function (token, slackBotToken, userConfig) {
         throw new Error('You must provide a Slack bot token!');
     this.slackBotToken = slackBotToken;
     // Merge user configuration into the default config
-    this._config = Object.assign({debug: false}, userConfig);
+    this._config = Object.assign({
+        debug: false,
+        baseUrl : "https://api.botanalytics.co/v1"
+    }, userConfig);
     //state definers
     this._isInited = false;
     this._isFetched = false;
     //logger
-    this.logger = new require('../../util').Logger(this._config);
-    this.logger.debug('Logging enabled.');
-    this.logger.debug('Configuration: ' + util.inspect(this._config));
+    this._logger = new require('../../util').Logger(this._config);
+    this._logger.debug('Logging enabled.');
+    this._logger.debug('Configuration: ' + util.inspect(this._config));
 
     this._init = function (data) {
 
@@ -29,7 +32,7 @@ module.exports = function (token, slackBotToken, userConfig) {
             //send
             req({
 
-                url: 'https://api.botanalytics.co/v1/bots/slack/initialize/',
+                url: self._config.baseUrl+'/bots/slack/initialize/',
                 method: 'POST',
                 json: true,
                 headers: {
@@ -43,8 +46,8 @@ module.exports = function (token, slackBotToken, userConfig) {
             }, (err, resp, payload) => {
 
                 if(err) {
-                    self.logger.error('Failed to log init message.', err);
-                    self.logger.debug('Retrying...');
+                    self._logger.error('Failed to log init message.', err);
+                    self._logger.debug('Retrying...');
                     setTimeout(self.init.bind(self, data), 10000);
                 }
                 else{
@@ -60,8 +63,10 @@ module.exports = function (token, slackBotToken, userConfig) {
             // check rtm start
             req.post({url : "https://slack.com/api/rtm.start", form : {token : self.slackBotToken}}, function (err, resp, body) {
 
-                if(JSON.parse(body).ok === false)
+                if(JSON.parse(body).ok === false) {
                     setTimeout(self._fetch.bind(self), 10000);
+                    self._logger.debug(body);
+                }
                 else {
                     self._init(body);
                     self._isFetched = true;
@@ -76,23 +81,23 @@ module.exports = function (token, slackBotToken, userConfig) {
 
         if (!message || message.constructor !== Object) {
 
-            this.logger.error('Message object is not provided!');
+            this._logger.error('Message object is not provided!');
             return;
         }
 
         if(message.type === "url_verification" || message.type !== "event_callback"){
-            this.logger.debug(`Ignoring ${message.type} message.`);
+            this._logger.debug(`Ignoring ${message.type} message.`);
             return;
         }
         const self = this;
         //log incoming message
-        self.logger.debug('Logging incoming message...\n' + util.inspect(message));
+        self._logger.debug('Logging incoming message...\n' + util.inspect(message));
         // create copy of the message
         const payload = Object.assign({}, message);
         //send
         req({
 
-            url: 'https://api.botanalytics.co/v1/messages/slack/eventapi',
+            url: self._config.baseUrl+'/messages/slack/eventapi/',
             method: 'POST',
             json: true,
             headers: {
@@ -107,15 +112,15 @@ module.exports = function (token, slackBotToken, userConfig) {
 
             if (err) {
 
-                self.logger.error('Failed to log incoming message.', err);
+                self._logger.error('Failed to log incoming message.', err);
 
             } else {
 
-                err = self.logger.checkResponse(resp, 'Successfully logged incoming message.', 'Failed to log incoming message.');
+                err = self._logger.checkResponse(resp, 'Successfully logged incoming message.', 'Failed to log incoming message.');
 
                 if (err) {
 
-                    self.logger.error('Failed to log incoming message.', err);
+                    self._logger.error('Failed to log incoming message.', err);
                 }
             }
         });
