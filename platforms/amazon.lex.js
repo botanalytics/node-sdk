ba153d25549f65d76fdfd3092bf9d04e2cdd4205
Beyhan Esen <beyhan@botanalytics.co>
2018-03-19 14:28:54 +0300

    Lex handler function is added.

--- platforms/amazon.lex.js
+++ platforms/amazon.lex.js
@@ -99,7 +99,9 @@ module.exports = function(token, userConfig) {
                     "intent_confirmation_status" : objectPath.get(event, "currentIntent.confirmationStatus","")
                 }
             };
+            
             logger.debug("Logging...");
+
             request({
 
                 url: '/messages/amazon-lex/',
@@ -109,13 +111,85 @@ module.exports = function(token, userConfig) {
                     message: payload
                 }
 
-            }, (err) => {
+            }, (err,resp) => {
 
-                if (err)
-                    logger.error('Failed to log message.', err);
-                else
-                    logger.debug('Success...');
+                if (err) {
+
+                    logger.error('Failed to log incoming message.', err);
+
+                } else {
+
+                    err = logger.checkResponse(resp, 'Successfully logged incoming message.', 'Failed to log incoming message.');
+
+                    if(err){
+                        logger.error("Failed to log.", err);
+                    }
+                }
             });
+        },
+        handler : function (handler) {
+
+            if(!handler || handler.constructor !== Function){
+                throw new Error("Handler function is not provided!");
+            }
+
+            let event, context, callback;
+
+            const rCatch = function (a, response) {
+
+                logger.debug('Preparing payload...');
+
+                const payload = {
+                    "user_id" : objectPath.get(event, "userId", null),
+                    "user_content" : objectPath.get(event, "inputTranscript", ""),
+                    "bot_content" : getResponseContent(response),
+                    "user_request_type" : objectPath.get(event, "outputDialogMode") === "Text" ? "text" : "voice",
+                    "bot_response_type" : getResponseType(response),
+                    "request" : {
+                        "id" : context.awsRequestId,
+                        "attributes" : objectPath.get(event, "requestAttributes", {}),
+                        "state" : objectPath.get(response, "dialogAction.fulfillmentState", ""),
+                        "bot_name" : objectPath.get(event, "bot.name",""),
+                        "intent_resolved_name" : objectPath.get(event, "currentIntent.name", ""),
+                        "intent_applied_name" : objectPath.get(response, "dialogAction.intentName", ""),
+                        "intent_confirmation_status" : objectPath.get(event, "currentIntent.confirmationStatus","")
+                    }
+                };
+
+                logger.debug("Logging...");
+
+                request({
+
+                    url: '/messages/amazon-lex/',
+                    method: 'POST',
+                    json: true,
+                    body: {
+                        message: payload
+                    }
+
+                }, (err,resp) => {
+
+                    if (err) {
+
+                        logger.error('Failed to log incoming message.', err);
+
+                    } else {
+
+                        err = logger.checkResponse(resp, 'Successfully logged incoming message.', 'Failed to log incoming message.');
+
+                        if(err){
+                            logger.error("Failed to log.", err);
+                        }
+                    }
+                });
+                callback(a, response);
+            };
+            return function (_event, _context, _callback) {
+                event = _event;
+                context = _context;
+                callback = _callback;
+                handler(_event, _context, rCatch);
+            }
         }
     };
 };
