aaff8c874bbd8fa234c1e0b61d6c1c6bce586347
Beyhan Esen <beyhan@botanalytics.co>
2018-04-02 12:43:19 +0300

    Lex updates.

--- platforms/amazon.lex.js
+++ platforms/amazon.lex.js
@@ -133,52 +133,57 @@ module.exports = function(token, userConfig) {
 
             let event, context, callback;
 
-            const rCatch = function (a, response) {
+            const rCatch = function (error, response) {
 
-                logger.debug('Preparing payload...');
+                if(!error) {
+                    logger.debug('Preparing payload...');
 
-                const payload = {
-                    "user_id" : objectPath.get(event, "userId", null),
-                    "user_content" : objectPath.get(event, "inputTranscript", ""),
-                    "bot_content" : getResponseContent(response),
-                    "user_request_type" : objectPath.get(event, "outputDialogMode") === "Text" ? "text" : "voice",
-                    "bot_response_type" : getResponseType(response),
-                    "request" : {
-                        "id" : context.awsRequestId,
-                        "attributes" : objectPath.get(event, "requestAttributes", {}),
-                        "state" : objectPath.get(response, "dialogAction.fulfillmentState", ""),
-                        "bot_name" : objectPath.get(event, "bot.name",""),
-                        "intent_resolved_name" : objectPath.get(event, "currentIntent.name", ""),
-                        "intent_applied_name" : objectPath.get(response, "dialogAction.intentName", ""),
-                        "intent_confirmation_status" : objectPath.get(event, "currentIntent.confirmationStatus","")
-                    }
-                };
-
-                logger.debug("Logging...");
-
-                request({
-
-                    url: '/messages/amazon-lex/',
-                    method: 'POST',
-                    json: true,
-                    body: payload
-
-                }, (err,resp) => {
-
-                    if (err) {
-
-                        logger.error('Failed to log incoming message.', err);
-
-                    } else {
-
-                        err = logger.checkResponse(resp, 'Successfully logged incoming message.', 'Failed to log incoming message.');
-
-                        if(err){
-                            logger.error("Failed to log.", err);
+                    const payload = {
+                        "user_id": objectPath.get(event, "userId", null),
+                        "user_content": objectPath.get(event, "inputTranscript", ""),
+                        "bot_content": getResponseContent(response),
+                        "user_request_type": objectPath.get(event, "outputDialogMode") === "Text" ? "text" : "voice",
+                        "bot_response_type": getResponseType(response),
+                        "request": {
+                            "id": context.awsRequestId,
+                            "attributes": objectPath.get(event, "requestAttributes", {}),
+                            "state": objectPath.get(response, "dialogAction.fulfillmentState", ""),
+                            "bot_name": objectPath.get(event, "bot.name", ""),
+                            "intent_resolved_name": objectPath.get(event, "currentIntent.name", ""),
+                            "intent_applied_name": objectPath.get(response, "dialogAction.intentName", ""),
+                            "intent_confirmation_status": objectPath.get(event, "currentIntent.confirmationStatus", "")
                         }
-                    }
-                });
-                callback(a, response);
+                    };
+
+                    logger.debug("Logging...");
+
+                    request({
+
+                        url: '/messages/amazon-lex/',
+                        method: 'POST',
+                        json: true,
+                        body: payload
+
+                    }, (err, resp) => {
+
+                        if (err) {
+
+                            logger.error('Failed to log incoming message.', err);
+
+                        } else {
+
+                            err = logger.checkResponse(resp, 'Successfully logged incoming message.', 'Failed to log incoming message.');
+
+                            if (err) {
+                                logger.error("Failed to log.", err);
+                            }
+                        }
+                    });
+                }
+                else
+                    logger.debug('Not logged due to error!');
+
+                callback(error, response);
             };
             return function (_event, _context, _callback) {
                 event = _event;
