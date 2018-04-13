36ec48e5faffd64aadd2fac1ccaf37bf4d11d986
Beyhan Esen <beyhan@botanalytics.co>
2018-04-13 15:54:22 +0300

    Google attach method is changed.

--- platforms/google.js
+++ platforms/google.js
@@ -3,9 +3,9 @@ const util = require('util');
 
 const payloadSanity = function(req, res) {
 
-	if(!req)
-		return {ok:false, err : new Error(`No request data: ${util.inspect(req)}`)};
-	if(!res)
+    if(!req)
+        return {ok:false, err : new Error(`No request data: ${util.inspect(req)}`)};
+    if(!res)
         return {ok:false, err : new Error(`No response data: ${util.inspect(res)}`)};
     if(req.constructor !== Object)
         return {ok:false, err : new Error(`Request data is not an object : ${util.inspect(req)}`)};
@@ -13,108 +13,92 @@ const payloadSanity = function(req, res) {
         return {ok:false, err : new Error(`Response data is not an object : ${util.inspect(res)}`)};
     if(!req.user || !req.conversation || !req.inputs)
         return {ok:false, err : new Error(`Request data is missing one or more of the required fields user, conversation, inputs : ${util.inspect(res)}`)};
-	if(!(res.expectedInputs || res.finalResponse))
+    if(!(res.expectedInputs || res.finalResponse))
         return {ok:false, err : new Error(`Response data is missing one or more of the required finalResponse or expectedInputs : ${util.inspect(req)}`)};
-	return {ok:true, err:null};
+    return {ok:true, err:null};
 };
 
 module.exports = function(token, userConfig) {
 
-	// Check token
-	if (!token) {
+    // Check token
+    if (!token) {
 
-		throw new Error('You must provide a Botanalytics token!');
-	}
+        throw new Error('You must provide a Botanalytics token!');
+    }
 
-	// Create default config
-	const config = {
-		baseUrl: 'https://api.botanalytics.co/v1/',
-		debug: false
-	};
+    // Create default config
+    const config = {
+        baseUrl: 'https://api.botanalytics.co/v1/',
+        debug: false
+    };
 
-	// Merge user configuration into the default config
-	Object.assign(config, userConfig);
+    // Merge user configuration into the default config
+    Object.assign(config, userConfig);
 
-	const log = new require('../util').Logger(config);
+    const log = new require('../util').Logger(config);
 
-	log.debug('Logging enabled.');
+    log.debug('Logging enabled.');
 
-	log.debug('Configuration: ' + util.inspect(config))
+    log.debug('Configuration: ' + util.inspect(config))
 
-	// Configure request defaults
-	const request = require('request').defaults({
-		baseUrl: config.baseUrl,
-		headers: {
-			'Authorization': 'Token ' + encodeURIComponent(token),
-			'Content-Type': 'application/json'
-		}
-	});
+    // Configure request defaults
+    const request = require('request').defaults({
+        baseUrl: config.baseUrl,
+        headers: {
+            'Authorization': 'Token ' + encodeURIComponent(token),
+            'Content-Type': 'application/json'
+        }
+    });
 
 
-	return {
-		log : (req, res) => {
-			//check sanity
-			const sanity = payloadSanity(req, res);
-			//process
-			if(sanity.ok)
+    return {
+        log : (req, res) => {
+            //check sanity
+            const sanity = payloadSanity(req, res);
+            //process
+            if(sanity.ok)
 
-				request({
+                request({
 
-					url: '/messages/google-assistant/',
-					method: 'POST',
-					json: true,
-					body: {
-						request : req,
-						response: res
-					}
+                    url: '/messages/google-assistant/',
+                    method: 'POST',
+                    json: true,
+                    body: {
+                        request : req,
+                        response: res
+                    }
 
-				}, (err, resp, payload) => {
+                }, (err, resp, payload) => {
 
-					if (err) {
+                    if (err) {
 
-						log.error('Failed to log message.', err);
-						return;
-					}
+                        log.error('Failed to log message.', err);
+                        return;
+                    }
 
-					err = log.checkResponse(resp, 'Successfully logged messages.', 'Failed to log messages.');
-					if(err)
+                    err = log.checkResponse(resp, 'Successfully logged messages.', 'Failed to log messages.');
+                    if(err)
                         log.error("Failed to log message", err);
                 });
-			else
-				log.error("Failed to process messages.", sanity.err);
-		},
-		attach: (Assistant, config, callback) => {
+            else
+                log.error("Failed to process messages.", sanity.err);
+        },
+        attach: (Assistant, callback) => {
 
-			if(!config.request || !config.response){
-				const err = new Error("Empty request or response object.");
-				log.error("Can not be attached!", err );
-				if(callback)
-					callback(err);
-				return new Assistant(config);
-			}
+            const assistant = new Assistant(config);
+            assistant.originalDoResponse = assistant.doResponse_;
+            assistant.doResponse_ = function (responseData, responseCode) {
 
-			if(config.response.send.constructor !== Function){
-                const err = new Error("Response is not an express js response object");
-                log.error("Can not be attached!", err );
-                if(callback)
-                    callback(err);
-                return new Assistant(config);
-			}
+                const sanity = payloadSanity(assistant.body_, responseData);
 
-			this.assistant = new Assistant(config);
-			this.assistant.originalDoResponse = this.assistant.doResponse_;
-			this.requestData = this.assistant.body_;
-			this.assistant.doResponse_ = function (responseData, responseCode) {
-
-                const sanity = payloadSanity(this.assistant.body_, responseData);
-				if(sanity.ok)
+                if(sanity.ok)
                     request({
 
                         url: '/messages/google-assistant/',
                         method: 'POST',
                         json: true,
                         body: {
-                            request : this.requestData,
+                            request : assistant.body_,
                             response: responseData
                         }
 
@@ -122,23 +106,31 @@ module.exports = function(token, userConfig) {
 
                         if (err) {
 
-                            log.error('Failed to log message.', err);
+                            log.debug(`Failed to log message. Reason: ${err.message}`);
+                            if(callback)
+                            	callback(err);
                             return;
                         }
 
                         err = log.checkResponse(resp, 'Successfully logged messages.', 'Failed to log messages.');
 
-                        if(err)
-                            log.error('Failed to log messages.', err);
+                        if(err){
+                            log.debug(`Failed to log message. Reason: ${err.message}`);
+							if (callback)
+								callback(err)
+                        }
                     });
-                else
-                    log.error('Failed to log messages', sanity.err);
+                else{
+                    log.error(`Failed to log messages. Reason: ${sanity.err.message}`);
+                    if(callback)
+                    	callback(sanity.err)
+                }
 
-                this.assistant.originalDoResponse(responseData, responseCode);
+                assistant.originalDoResponse(responseData, responseCode);
 
             };
 
-			return new Assistant(config);
-		}
-	};
+            return assistant;
+        }
+    };
 };
