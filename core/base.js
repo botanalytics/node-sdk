4c91b6ca66f9fc0e8a78342758cdcb964f004610
Deniz Acay <deniz@denizacay.com>
2022-06-10 21:48:39 +0300

    feat(core): Add basic validation for API keys.

--- core/base.js
+++ core/base.js
@@ -16,28 +16,37 @@ export default class Base {
 
     constructor(options) {
 
-
-        // Check for channel
+        // Check for API key
         if (!process.env.BA_API_KEY && (!options || !options.apiKey)) {
 
-            logger.error("API key parameter is required.");
+            logger.error("API key parameter is required.")
 
-            process.exit(1);
+            throw new Error("API key parameter is required.")
         }
 
+        // Check for channel
+        if (!options || !options._channel)
+            throw new Error("Internal inconsistency.")
+
         // Store arguments
-        this.apiKey = process.env.BA_API_KEY || options.apiKey;
-        this.baseUrl = process.env.BA_BASE_URL || (options && options.baseUrl) || defaultBaseUrl;
+        this._apiKey = process.env.BA_API_KEY || options.apiKey;
+        this._baseUrl = process.env.BA_BASE_URL || (options && options.baseUrl) || defaultBaseUrl;
+        this._channel = options._channel;
+
+        // Store logger
         this.logger = logger;
 
+        // Basic validation for API key
+        this._validateApiKey()
+
         // Create HTTP client instance
-        this.httpClient = got.extend({
-            prefixUrl: this.baseUrl,
+        this._httpClient = got.extend({
+            prefixUrl: this._baseUrl,
             method: 'POST',
             responseType: 'json',
             followRedirect: false,
             headers: {
-                'Authorization': 'Bearer ' + this.apiKey
+                'Authorization': 'Bearer ' + this._apiKey
             },
             retry: {
                 limit: (process.env.BA_REQUEST_RETRY_LIMIT && parseInt(process.env.BA_REQUEST_RETRY_LIMIT)) || (options && options.requestRetryLimit) || defaultRequestRetryLimit,
@@ -81,5 +90,54 @@ export default class Base {
                 ]
             }
         });
+    }
+
+    _validateApiKey() {
+
+        // Split token
+        let tokenParts = this._apiKey.split('.');
+
+        // Check token parts size
+        if (!tokenParts || tokenParts.length < 2) {
+
+            logger.error("API key is not a valid JWT.")
+
+            throw new Error("API key is not a valid JWT.")
+        }
+
+        // Get the first part
+        let firstPart = tokenParts[1];
+
+        // Convert token JSON to string
+        let tokenStr = Buffer.from(firstPart, 'base64').toString('utf-8')
+
+        // Parse token string
+        let tokenJson;
+        try {
+
+            tokenJson = JSON.parse(tokenStr);
+
+        } catch (err) {
+
+            logger.error("API key is not a valid JWT.")
+
+            throw new Error("API key is not a valid JWT.")
+        }
+
+        // Check if channel field exists
+        if (!tokenJson || !tokenJson.channel) {
+
+            logger.error("API key is missing a channel claim.")
+
+            throw new Error("API key is missing a channel claim.")
+        }
+
+        // Check if channel field matches
+        if (tokenJson.channel !== this._channel) {
+
+            logger.error("API key does not match the client channel.")
+
+            throw new Error("API key does not match the client channel.")
+        }
     }
 }
