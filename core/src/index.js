124f060aeeb3f1ab8c97cbfa453b2007c21cf345
Deniz Acay <deniz@denizacay.com>
2022-06-18 05:46:29 +0300

    feat(core): Add MicrosoftBotFrameworkClient.

--- core/src/index.js
+++ core/src/index.js
@@ -1,3 +1,4 @@
 export { default as UniversalClient } from './universal.js';
 export { default as FacebookMessengerClient } from './facebook-messenger.js';
 export { default as AmazonAlexaClient } from './amazon-alexa.js';
+export { default as MicrosoftBotFrameworkClient } from './microsoft-bot-framework.js';
